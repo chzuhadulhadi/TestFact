711ab6ebdfe5771c224789691b02d0702d7bde6a
aliumair90 <aliumair.cs90@gmail.com>
2024-09-10 23:08:17 +0500

    Comment preview page

--- Frontend/src/pages/Testee/MyTest/components/AllSteps/Preview.js
+++ Frontend/src/pages/Testee/MyTest/components/AllSteps/Preview.js
@@ -1,71 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; // For extracting URL parameters
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; // For extracting URL parameters
 import FillQuestion from "../../../../questionaire/components/fillquestion";
-import { apiCall } from '../../../../../apiCalls/apiCalls';
-import { getMySingleTest } from '../../../../../apiCalls/apiRoutes';
-import SideBar from '../../../../mainComponent/SideBar';
+import { apiCall } from "../../../../../apiCalls/apiCalls";
+import { getMySingleTest } from "../../../../../apiCalls/apiRoutes";
+import SideBar from "../../../../mainComponent/SideBar";
 
 // Example API function to fetch data based on ID
 
-
 const Preview = () => {
-    const { id } = useParams(); // Extracts the 'id' from the URL
-    const [data, setData] = useState(null);
+  const { id } = useParams(); // Extracts the 'id' from the URL
+  const [data, setData] = useState(null);
 
-    const fetchDataById = async (id) => {
-        apiCall("post", getMySingleTest, { id: id })
-          .then((response) => {
-            if (response.status == 200) {
-              // console.log(response?.data?.data?.rows[0]);
-              // const converted = convertData(response?.data?.data?.rows[0]);
-              // console.log(converted);
-              // mainObj = {
-              // ...response?.data?.data?.rows[0],
-              // ...converted
-              // };
-              const data = response?.data?.data;
-              console.log(data);
-              setData({
-                id: data.obj.id,
-                sendAll: !!data.sendAll,
-                orientation: data.orientation,
-                scoringType: data.scoringType,
-                randomOrder: data.randomOrder,
-                timeLimit: data.timeLimit,
-                questions: data.questions,
-                resultStructure: data.resultStructure,
-                automaticText: data.automaticText,
-                freeText: data.freeText,
-                beforeTestText: data.beforeTestText,
-                afterTestText: data.afterTestText,
-                name: data.name,
-                categoryStore: data.categoryStore,
-                layout: data.layout,
-                testObj:data.getobj,
-              });
-            }
+  const fetchDataById = async (id) => {
+    apiCall("post", getMySingleTest, { id: id }).then((response) => {
+      if (response.status == 200) {
+        // console.log(response?.data?.data?.rows[0]);
+        // const converted = convertData(response?.data?.data?.rows[0]);
+        // console.log(converted);
+        // mainObj = {
+        // ...response?.data?.data?.rows[0],
+        // ...converted
+        // };
+        const data = response?.data?.data;
+        console.log(data);
+        setData({
+          id: data.obj.id,
+          sendAll: !!data.sendAll,
+          orientation: data.orientation,
+          scoringType: data.scoringType,
+          randomOrder: data.randomOrder,
+          timeLimit: data.timeLimit,
+          questions: data.questions,
+          resultStructure: data.resultStructure,
+          automaticText: data.automaticText,
+          freeText: data.freeText,
+          beforeTestText: data.beforeTestText,
+          afterTestText: data.afterTestText,
+          name: data.name,
+          categoryStore: data.categoryStore,
+          layout: data.layout,
+          testObj: data.getobj,
         });
-    };
+      }
+    });
+  };
 
-    useEffect(() => {
-        if (id) {
-            const getData = async () => {
-                try {
-                    const result = await fetchDataById(id);
-                    setData(result); // Save the data in state
-                } catch (error) {
-                    console.error('Error fetching data:', error);
-                }
-            };
-            getData();
+  useEffect(() => {
+    if (id) {
+      const getData = async () => {
+        try {
+          const result = await fetchDataById(id);
+          setData(result); // Save the data in state
+        } catch (error) {
+          console.error("Error fetching data:", error);
         }
-    }, [id]); // Re-fetch data if the ID changes
+      };
+      getData();
+    }
+  }, [id]); // Re-fetch data if the ID changes
 
-    return (
-        
-        <>
-        <SideBar/>
-        <div id="content" className=" w-auto sm:w-auto">
+  return (
+    <>
+      <SideBar />
+      {/* <div id="content" className=" w-auto sm:w-auto">
             {data?.testObj && (
                 <div className="w-[80%] mx-auto">
                     <div className="flex justify-center mt-8">
@@ -77,9 +74,9 @@ const Preview = () => {
                     />
                 </div>
             )}
-            </div>
-        </>
-    );
+            </div> */}
+    </>
+  );
 };
 
 export default Preview;
